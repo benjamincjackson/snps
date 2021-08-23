f39d35de1d877bc8e1d9e84b07c245bbe1f6e7d2
benjamincjackson <benjamincjackson@gmail.com>
2021-08-23 12:20:23 +0100

    adds the ability not to treat alignment gaps as missing data

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/benjamincjackson/snps
+
+go 1.16
+
+require github.com/spf13/cobra v1.2.1
--- snps.go
+++ snps.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"log"
 	"bufio"
-	"os"
-	"strings"
 	"errors"
+	"os"
 	"runtime"
-	"sync"
 	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/spf13/cobra"
 )
 
 // encodedFastaRecord is a struct for one Fasta record
@@ -16,14 +17,14 @@ type encodedFastaRecord struct {
 	ID          string
 	Description string
 	Seq         []byte
-	idx int
+	idx         int
 }
 
 // snpLine is a struct for one Fasta record's SNPs
 type snpLine struct {
 	queryname string
-	snps []string
-	idx int
+	snps      []string
+	idx       int
 }
 
 // makeEncodingArray returns an array whose indices are the byte representations
@@ -68,6 +69,48 @@ func makeEncodingArray() []byte {
 	return byteArray
 }
 
+// makeEncodingArray returns an array whose indices are the byte representations
+// of IUPAC codes and whose contents are Emmanual Paradis encodings
+// Lower case nucleotides are mapped to their upper case nucleotides's encoding
+func makeEncodingArrayHardGaps() []byte {
+	byteArray := make([]byte, 256)
+
+	byteArray['A'] = 136
+	byteArray['a'] = 136
+	byteArray['G'] = 72
+	byteArray['g'] = 72
+	byteArray['C'] = 40
+	byteArray['c'] = 40
+	byteArray['T'] = 24
+	byteArray['t'] = 24
+	byteArray['R'] = 192
+	byteArray['r'] = 192
+	byteArray['M'] = 160
+	byteArray['m'] = 160
+	byteArray['W'] = 144
+	byteArray['w'] = 144
+	byteArray['S'] = 96
+	byteArray['s'] = 96
+	byteArray['K'] = 80
+	byteArray['k'] = 80
+	byteArray['Y'] = 48
+	byteArray['y'] = 48
+	byteArray['V'] = 224
+	byteArray['v'] = 224
+	byteArray['H'] = 176
+	byteArray['h'] = 176
+	byteArray['D'] = 208
+	byteArray['d'] = 208
+	byteArray['B'] = 112
+	byteArray['b'] = 112
+	byteArray['N'] = 240
+	byteArray['n'] = 240
+	byteArray['-'] = 4
+	byteArray['?'] = 242
+
+	return byteArray
+}
+
 // makeDecodingArray returns an array whose indices are Emmanual Paradis encodings
 // of IUPAC codes and whose contents are IUPAC codes as strings
 func makeDecodingArray() []string {
@@ -94,9 +137,33 @@ func makeDecodingArray() []string {
 	return byteArray
 }
 
+func makeDecodingArrayHardGaps() []string {
+	byteArray := make([]string, 256)
+
+	byteArray[136] = "A"
+	byteArray[72] = "G"
+	byteArray[40] = "C"
+	byteArray[24] = "T"
+	byteArray[192] = "R"
+	byteArray[160] = "M"
+	byteArray[144] = "W"
+	byteArray[96] = "S"
+	byteArray[80] = "K"
+	byteArray[48] = "Y"
+	byteArray[224] = "V"
+	byteArray[176] = "H"
+	byteArray[208] = "D"
+	byteArray[112] = "B"
+	byteArray[240] = "N"
+	byteArray[4] = "-"
+	byteArray[242] = "?"
+
+	return byteArray
+}
+
 // readEncodeAlignment reads an alignment in fasta format to a channel
 // of encodedFastaRecord structs - converting sequence to EP's bitwise coding scheme
-func readEncodeAlignment(infile string, chnl chan encodedFastaRecord, chnlerr chan error, cdone chan bool) {
+func readEncodeAlignment(infile string, hardGaps bool, chnl chan encodedFastaRecord, chnlerr chan error, cdone chan bool) {
 
 	f, err := os.Open(infile)
 
@@ -106,8 +173,13 @@ func readEncodeAlignment(infile string, chnl chan encodedFastaRecord, chnlerr ch
 
 	defer f.Close()
 
-	encoding := makeEncodingArray()
-	// encoding := MakeByteDict2()
+	var encoding []byte
+	switch hardGaps {
+	case true:
+		encoding = makeEncodingArrayHardGaps()
+	case false:
+		encoding = makeEncodingArray()
+	}
 
 	s := bufio.NewScanner(f)
 
@@ -146,7 +218,7 @@ func readEncodeAlignment(infile string, chnl chan encodedFastaRecord, chnlerr ch
 
 		} else {
 			encodedLine := make([]byte, len(line))
-			for i := range(line) {
+			for i := range line {
 				encodedLine[i] = encoding[line[i]]
 			}
 			seqBuffer = append(seqBuffer, encodedLine...)
@@ -165,23 +237,29 @@ func readEncodeAlignment(infile string, chnl chan encodedFastaRecord, chnlerr ch
 }
 
 // getSNPs gets the SNPs between the reference and each Fasta record at a time
-func getSNPs(refSeq []byte, cFR chan encodedFastaRecord, cSNPs chan snpLine, cErr chan error) {
+func getSNPs(refSeq []byte, hardGaps bool, cFR chan encodedFastaRecord, cSNPs chan snpLine, cErr chan error) {
 
-	DA := makeDecodingArray()
+	var DA []string
+	switch hardGaps {
+	case true:
+		DA = makeDecodingArrayHardGaps()
+	case false:
+		DA = makeDecodingArray()
+	}
 
-	for FR := range(cFR) {
+	for FR := range cFR {
 		SL := snpLine{}
 		SL.queryname = FR.ID
 		SL.idx = FR.idx
 		SNPs := make([]string, 0)
-		for i, nuc := range(FR.Seq) {
+		for i, nuc := range FR.Seq {
 			if (refSeq[i] & nuc) < 16 {
-				snpLine := DA[refSeq[i]] + strconv.Itoa(i + 1) + DA[nuc]
+				snpLine := DA[refSeq[i]] + strconv.Itoa(i+1) + DA[nuc]
 				SNPs = append(SNPs, snpLine)
 			}
 		}
 		SL.snps = SNPs
-		cSNPs<- SL
+		cSNPs <- SL
 	}
 
 	return
@@ -189,16 +267,27 @@ func getSNPs(refSeq []byte, cFR chan encodedFastaRecord, cSNPs chan snpLine, cEr
 
 // writeOutput writes the output to stdout as it arrives. It uses a map to write things
 // in the same order as they are in the input file.
-func writeOutput(cSNPs chan snpLine, cErr chan error, cWriteDone chan bool) {
+func writeOutput(outFile string, cSNPs chan snpLine, cErr chan error, cWriteDone chan bool) {
 
 	outputMap := make(map[int]snpLine)
 
 	counter := 0
 
-	f := os.Stdout
+	var f *os.File
+	var err error
+
+	if outFile != "stdout" {
+		f, err = os.Create(outFile)
+		if err != nil {
+			cErr <- err
+		}
+	} else {
+		f = os.Stdout
+	}
+
 	defer f.Close()
 
-	_, err := f.WriteString("query,SNPs\n")
+	_, err = f.WriteString("query,SNPs\n")
 	if err != nil {
 		cErr <- err
 	}
@@ -236,90 +325,116 @@ func writeOutput(cSNPs chan snpLine, cErr chan error, cWriteDone chan bool) {
 }
 
 // Run the program
-func main() {
+func snps(alignmentFile string, referenceFile string, hardGaps bool, outFile string) error {
 
-	if len(os.Args) != 3 {
-		os.Stderr.WriteString("Usage: ./snps reference.fasta alignment.fasta > snps.csv\n")
-	} else {
-		referenceFile := os.Args[1]
-		alignmentFile := os.Args[2]
+	cErr := make(chan error)
 
-		cErr := make(chan error)
+	cRef := make(chan encodedFastaRecord)
+	cRefDone := make(chan bool)
 
-		cRef := make(chan encodedFastaRecord)
-		cRefDone := make(chan bool)
+	cFR := make(chan encodedFastaRecord)
+	cFRDone := make(chan bool)
 
-		cFR := make(chan encodedFastaRecord)
-		cFRDone := make(chan bool)
+	cSNPs := make(chan snpLine, runtime.NumCPU())
+	cSNPsDone := make(chan bool)
 
-		cSNPs := make(chan snpLine, runtime.NumCPU())
-		cSNPsDone := make(chan bool)
+	cWriteDone := make(chan bool)
 
+	go readEncodeAlignment(referenceFile, hardGaps, cRef, cErr, cRefDone)
 
-		cWriteDone := make(chan bool)
+	var refSeq []byte
 
-		go readEncodeAlignment(referenceFile, cRef, cErr, cRefDone)
-
-		var refSeq []byte
-
-		for n := 1; n > 0; {
-			select {
-			case err := <-cErr:
-				log.Fatal(err)
-			case FR := <-cRef:
-				refSeq = FR.Seq
-			case <-cRefDone:
-				close(cRef)
-				n--
-			}
-		}
-
-		go readEncodeAlignment(alignmentFile, cFR, cErr, cFRDone)
-
-		go writeOutput(cSNPs, cErr, cWriteDone)
-
-		var wgSNPs sync.WaitGroup
-		wgSNPs.Add(runtime.NumCPU())
-
-		for n := 0; n < runtime.NumCPU(); n++ {
-			go func() {
-				getSNPs(refSeq, cFR, cSNPs, cErr)
-				wgSNPs.Done()
-			}()
-		}
-
-		go func() {
-			wgSNPs.Wait()
-			cSNPsDone<- true
-		}()
-
-		for n := 1; n > 0; {
-			select {
-			case err := <-cErr:
-				log.Fatal(err)
-			case <-cFRDone:
-				close(cFR)
-				n--
-			}
-		}
-
-		for n := 1; n > 0; {
-			select {
-			case err := <-cErr:
-				log.Fatal(err)
-			case <-cSNPsDone:
-				close(cSNPs)
-				n--
-			}
-		}
-
-		for n := 1; n > 0; {
-			select {
-			case err := <-cErr:
-				log.Fatal(err)
-			case <-cWriteDone:
-				n--
-			}
+	for n := 1; n > 0; {
+		select {
+		case err := <-cErr:
+			return err
+		case FR := <-cRef:
+			refSeq = FR.Seq
+		case <-cRefDone:
+			close(cRef)
+			n--
 		}
 	}
+
+	go readEncodeAlignment(alignmentFile, hardGaps, cFR, cErr, cFRDone)
+
+	go writeOutput(outFile, cSNPs, cErr, cWriteDone)
+
+	var wgSNPs sync.WaitGroup
+	wgSNPs.Add(runtime.NumCPU())
+
+	for n := 0; n < runtime.NumCPU(); n++ {
+		go func() {
+			getSNPs(refSeq, hardGaps, cFR, cSNPs, cErr)
+			wgSNPs.Done()
+		}()
+	}
+
+	go func() {
+		wgSNPs.Wait()
+		cSNPsDone <- true
+	}()
+
+	for n := 1; n > 0; {
+		select {
+		case err := <-cErr:
+			return err
+		case <-cFRDone:
+			close(cFR)
+			n--
+		}
+	}
+
+	for n := 1; n > 0; {
+		select {
+		case err := <-cErr:
+			return err
+		case <-cSNPsDone:
+			close(cSNPs)
+			n--
+		}
+	}
+
+	for n := 1; n > 0; {
+		select {
+		case err := <-cErr:
+			return err
+		case <-cWriteDone:
+			n--
+		}
+	}
+
+	return nil
+}
+
+var snpsReference string
+var snpsQuery string
+var snpsOutfile string
+var hardGaps bool
+
+func init() {
+	mainCmd.Flags().StringVarP(&snpsReference, "reference", "r", "", "Reference sequence, in fasta format")
+	mainCmd.Flags().StringVarP(&snpsQuery, "query", "q", "stdin", "Alignment of sequences to find snps in, in fasta format")
+	mainCmd.Flags().StringVarP(&snpsOutfile, "outfile", "o", "stdout", "Output to write")
+	mainCmd.Flags().BoolVarP(&hardGaps, "hard-gaps", "", false, "don't treat alignment gaps as missing data")
+
+	mainCmd.Flags().Lookup("hard-gaps").NoOptDefVal = "true"
+
+	mainCmd.Flags().SortFlags = false
+}
+
+var mainCmd = &cobra.Command{
+	Use:   "snps",
+	Short: "snps...",
+	Long:  `snps...`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+		err = snps(snpsQuery, snpsReference, hardGaps, snpsOutfile)
+
+		return
+	},
+}
+
+func main() {
+	mainCmd.Execute()
 }
